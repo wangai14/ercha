168e8caf0900be3ced17061e8ff635102fc02623
二叉树树 <acofork@qq.com>
2025-09-04 03:57:47 +0800

    fix: 更新状态页链接到新的uptimerobot地址

--- src/config.ts
+++ src/config.ts
@@ -78,7 +78,7 @@ export const navBarConfig: NavBarConfig = {
 		},
 		{
 			name: "状态",
-			url: "https://acoforkcloud-uptime.zeabur.app/status/acofork", // Internal links should not include the base path, as it is automatically added
+			url: "https://stats.uptimerobot.com/VAtAH0mzPN", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
